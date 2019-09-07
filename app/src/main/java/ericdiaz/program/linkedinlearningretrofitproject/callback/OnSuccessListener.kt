df70414f0a1c034ad846c26c77193193dfe1dc5c
Eric Diaz <ericdiaz@pursuit.org>
2019-09-07 19:57:17 -0400

    added get gist function to repository

--- app/src/main/java/ericdiaz/program/linkedinlearningretrofitproject/callback/OnSuccessListener.kt
+++ app/src/main/java/ericdiaz/program/linkedinlearningretrofitproject/callback/OnSuccessListener.kt
@@ -3,5 +3,6 @@ package ericdiaz.program.linkedinlearningretrofitproject.callback
 import ericdiaz.program.linkedinlearningretrofitproject.model.Gist
 
 interface OnSuccessListener {
-    fun onSuccess(gists: List<Gist>?)
+    fun onSuccess(gists: List<Gist>?){}
+    fun onSuccess(gist: Gist?){}
 }
