4fe68a5bba788487b17add56d35285603c411043
Eric Diaz <ericdiaz@pursuit.org>
2019-09-13 21:23:34 -0400

    added network methods to return Gist Comments with tests to get gist comments

--- app/src/main/java/ericdiaz/program/linkedinlearningretrofitproject/api/GithubService.kt
+++ app/src/main/java/ericdiaz/program/linkedinlearningretrofitproject/api/GithubService.kt
@@ -1,9 +1,12 @@
 package ericdiaz.program.linkedinlearningretrofitproject.api
 
 import ericdiaz.program.linkedinlearningretrofitproject.model.Gist
+import ericdiaz.program.linkedinlearningretrofitproject.model.GistComment
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.HEAD
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface GithubService {
 
@@ -25,6 +28,28 @@ interface GithubService {
     @GET("gists/starred")
     fun getStarredGists(): Call<List<Gist>>
 
+    /**
+     * Return a Single Gist
+     */
     @GET("gists/{gistId}")
     fun getGist(@Path("gistId") gistId: String): Call<Gist>
+
+    /**
+     * Return comments for a particular Gist provided by Id
+     */
+    @GET("gists/{gistId}/comments")
+    fun getGistCommentsById(
+        @Path("gistId") gistId: String,
+        @Query("page") pageNumber: Int
+    ): Call<List<GistComment>>
+
+    /**
+     * This method uses the head annotation which means that no body is required
+     * and is expected when you are making the call. This is why the return type
+     * inside of the call is of type Unit.
+     */
+    @HEAD("gists/{gistId}/comments")
+    fun getGistCommentHeadersById(@Path("gistId") gistId: String): Call<Void>
+
+
 }
--- app/src/test/java/ericdiaz.program.linkedinlearningretrofitproject/NetworkTest.kt
+++ app/src/test/java/ericdiaz.program.linkedinlearningretrofitproject/NetworkTest.kt
@@ -3,6 +3,7 @@ package ericdiaz.program.linkedinlearningretrofitproject
 import ericdiaz.program.linkedinlearningretrofitproject.api.GithubService
 import ericdiaz.program.linkedinlearningretrofitproject.api.RetrofitSingleton
 import ericdiaz.program.linkedinlearningretrofitproject.model.Gist
+import ericdiaz.program.linkedinlearningretrofitproject.model.GistComment
 import org.junit.Assert
 import org.junit.Test
 import retrofit2.Response
@@ -47,6 +48,35 @@ class NetworkTest {
 
         //then
         Assert.assertNotNull(response.body())
+        Assert.assertEquals(expectedCode, response.code())
+    }
+
+    @Test
+    fun testGetGistCommentsByIdFor200CodeWhenSynchronouslyCalled() {
+        //given
+        val expectedCode = 200
+        val testPageNumber = 1
+        val testGistId = "a946fd41fec95132614be5c9e4e32a5f"
+        //when
+        val response: Response<List<GistComment>> =
+            service.getGistCommentsById(testGistId, testPageNumber).execute()
+
+        //then
+        Assert.assertNotNull(response.body())
+        Assert.assertEquals(expectedCode, response.code())
+    }
+
+    @Test
+    fun testGetGistCommentsHeadersByIdFor200CodeWhenSynchronouslyCalled() {
+        //given
+        val expectedCode = 200
+        val testGistId = "a946fd41fec95132614be5c9e4e32a5f"
+        //when
+
+        val response: Response<Void> = service.getGistCommentHeadersById(testGistId).execute()
+
+        //then
+        Assert.assertNotNull(response.headers())
         Assert.assertEquals(expectedCode, response.code())
     }
 }
